185a7d2230e35e78728fbad2c4856c9ddc461c57
ubiquitous-aman <amankumaraman112@gmail.com>
2026-08-07 11:07:57 +0530

    test.js file

--- Registration/test.js
+++ Registration/test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let passed = true;
 
 console.log("Registration Test\n");
-// TC - 01 : index.html exists
+// TC - 01
 if (fs.existsSync("index.html")) {
     console.log("TC-01: index.html exists: PASS");
 }
@@ -41,6 +41,54 @@ else {
 const students = JSON.parse(fs.readFileSync("student.json"));
 const student = students[0];
 
+// TC - 05
 if (student.name.trim() !== "") {
+    console.log("TC-05: Name validation: PASS");
+}
+else {
+    console.log("TC-05: Name validation: FAIL");
+    passed = false;
+}
 
+// TC - 06
+if (student.email.includes('@')) {
+    console.log("TC-06: Email validation: PASS");
+}
+else {
+    console.log("TC-06: Email validation: FAIL");
+    passed = false;
+}
+
+// TC-07
+if (student.mobile.length === 10) {
+    console.log("TC-07: Mobile validation:PASS");
+}
+else {
+    console.log("TC-07: Mobile validation:FAIL");
+    passed = false;
+}
+
+//TC-08
+if (student.branch !== "") {
+    console.log("TC-08:Branch validation:PASS");
+}
+else {
+    console.log("TC-08:Branch validation:FAIL");
+    passed = false;
+}
+
+//TC-09
+if (student.password.length >= 6) {
+    console.log("TC-09:Password validation:PASS");
+}
+else {
+    console.log("TC-09:Password validation:FAIL");
+    passed = false;
+}
+
+//TC-10
+if (passed) {
+    console.log("TC-10:Registration successful:PASS");
+    console.log("\nBuild SUCCESS");
+    process.exit();
 }
